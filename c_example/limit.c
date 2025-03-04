8f6bd7ea783ce9d1265b68a7f7e54ae8c3cda869
wooseok-L2 <dntjr9009@knou.ac.kr>
2025-03-04 14:46:30 +0900

    limit.c

---
+++ c_example/limit.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //standard input/output function
+#include <limits.h>
+
+int main(){
+    
+    printf("Hello world\n");
+    printf("char_bit : %d\n", CHAR_BIT);
+    printf("char_max : %d\n", CHAR_MAX);
+
+    return 0;
+} 
