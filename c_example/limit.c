52b31647e1917176fea6d91cb922acd52b946f10
wooseok-L2 <dntjr9009@knou.ac.kr>
2025-03-05 09:46:50 +0900

    add contents on limit.c

--- c_example/limit.c
+++ c_example/limit.c
@@ -1,11 +1,20 @@
 #include <stdio.h> //standard input/output function
 #include <limits.h>
 
-int main(){
+void printHello();
+// int main(int argc, char* argv[])
+int main(void){
     
+    printHello();
     printf("Hello world\n");
     printf("char_bit : %d\n", CHAR_BIT);
     printf("char_max : %d\n", CHAR_MAX);
 
     return 0;
 } 
+
+void printHello(){
+    
+    printf("Hello, world \n");
+
+}
