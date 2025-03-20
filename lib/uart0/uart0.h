5b2dc048648ef0af81cae67cb636d40576213460
wooseok-L2 <dntjr9009@knou.ac.kr>
2025-03-20 15:48:04 +0900

    good

--- lib/uart0/uart0.h
+++ lib/uart0/uart0.h
@@ -7,6 +7,7 @@ void uart0Transmit(char data);
 unsigned char uart0Receive(void);
 void uart0PrintString(char *str);
 void uart0Print1ByteNumber(unsigned char n);
+
 FILE OUTPUT = FDEV_SETUP_STREAM(uart0Transmit, NULL, _FDEV_SETUP_WRITE);
 FILE INPUT = FDEV_SETUP_STREAM(NULL, uart0Receive, _FDEV_SETUP_READ);
 
