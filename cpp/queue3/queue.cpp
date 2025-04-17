4e176599f78324ae8054f3343e0fe8ddd7c1f4f3
wooseok-L2 <dntjr@knou.ac.kr>
2025-04-17 16:59:50 +0900

    2025-04-17_end

---
+++ cpp/queue3/queue.cpp
@@ -0,0 +1,58 @@
+#include "queue.h"
+#include <cstdio>
+// #include <stdlib.h>
+#include <cassert>
+
+const int Queue::QUEUE_SIZE = 100;
+
+Queue::Queue(int size)
+: front_(0), rear_(0),arr_(size)
+{
+    //assert(this->parr_);       // 디버깅모드에서만 작동  assert(pq->parr != 0);   릴리즈 모드에서는 assert 함수 사라짐
+}
+
+Queue::~Queue()
+{
+    //delete[] parr_;
+}
+
+int Queue::size() const
+{
+    return arr_.size();
+}
+
+int Queue::remain() const
+{
+    return arr_.size() - rear_;
+}
+
+bool Queue::isEmpty() const
+{
+    return rear_ == 0 || rear_ == front_;
+}
+
+bool Queue::isFull() const
+{
+    return rear_ == arr_.size();
+}
+
+void Queue::push(int data)
+{  
+    assert(!this->isFull());
+    
+    arr_[rear_] = data;
+    ++rear_;
+}
+
+int Queue::pop()
+{  
+    assert(rear_ != front_);
+    
+    int i = front_;
+
+    ++front_;
+    
+    return this->arr_[i];
+    
+}
+
---
+++ cpp/queue3/queue.h
@@ -0,0 +1,34 @@
+// #pragma once                  // at visual studio 
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "array.h"
+
+// #define QUEUESIZE 100
+
+class Queue{
+private:
+    // int array[QUEUESIZE];
+    // int *parr_;
+    // int size_;
+    Array arr_;
+    int rear_;
+    int front_;
+    static const int QUEUE_SIZE;
+
+    Queue(const Queue& );  // is not used
+    Queue& operator=(const Queue& ); // is not used
+
+public:
+    explicit Queue(int size = QUEUE_SIZE);    // 생성자 만들기
+    ~Queue();         // 소멸자 만들기
+    int size() const;
+    int remain() const;
+    bool isEmpty() const;
+    bool isFull() const;
+    void push(int data);         // func. declaration
+    int pop();
+};                                // struct stack -> Stack
+
+
+#endif
