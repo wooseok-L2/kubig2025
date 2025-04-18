88cc8962f69ea989e24e2e5e57e3dfed0a74c5e9
wooseok-L2 <dntjr@knou.ac.kr>
2025-04-18 16:29:15 +0900

    ff

---
+++ cpp/array2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "array.h"
+
+int main()
+{
+    int nums1[] = (1, 2, 3, 4, 5);
+    Array<int> arr1(nums1, 5);
+
+    for (int i = 0; i < arr1.size(); i++){
+        std::cout << arr1[i] << std::endl;
+    }
+
+    double nums2[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    Array<double> arr2(nums2, 5);
+
+    for (int i = 0; i < arr2.size(); i++){
+        std::cout << arr2[i] << std::endl;
+    }
+
+    return 0;
+
+}
