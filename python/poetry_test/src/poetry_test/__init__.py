57ac6d5f06d90a726a845368ada7d99cfbc9c4fc
wooseok-L2 <dntjr@knou.ac.kr>
2025-05-13 12:29:04 +0900

    2025-05-13_morning

---
+++ python/poetry_test/src/poetry_test/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.1."
+
+def greet(name):
+    return f"Hello, {name}"
---
+++ python/poetry_test/test_poetry.py
@@ -0,0 +1,11 @@
+import requests
+import numpy
+
+
+def main():
+    print("poetry source test")
+    print(requests.__version__)
+    print(numpy.__version__)
+    
+if __name__ == "__main__":
+    main()
